c4df83a4f64bf76ccecde225ebf9ecc953f79408
NezzCOL <jhonhernandezospino@gmail.com>
2023-08-24 23:49:34 -0500

    trabajo-dashboard

---
+++ validaciones/validar-index.php
@@ -0,0 +1,23 @@
+<?php
+if(!empty($_POST["btn"])){
+    
+    $conexion = mysqli_connect("localhost","root","","login");
+
+    $usuario = $_POST['usuario'];
+    $contraseña = $_POST['contraseña'];
+
+    $consulta = "SELECT * FROM usuarios where usuario = '$usuario' and password = '$contraseña' ";
+    $resultado = mysqli_query($conexion, $consulta);
+
+    $filas = mysqli_num_rows($resultado);
+
+    if($filas){
+        header("location: ../pagina/pagina-inicio.php");
+    }else{
+        echo "<div class='error'>Error, datos incorrectos</div>";
+    }
+
+    mysqli_free_result($resultado);
+    mysqli_close($conexion);
+}
+?>
