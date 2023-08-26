6f90c21804b0ac656a013c32a99108c699363adc
NezzCOL <jhonhernandezospino@gmail.com>
2023-08-26 07:34:59 -0500

    trabajo-dashboard

--- pagina/pagina-inicio.php
+++ pagina/pagina-inicio.php
@@ -4,8 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../css/style-pagina-inicio.css">
 </head>
 <body>
-    hola
+    <header class="content-header">
+        <nav class="navbar">
+            <div class="icon-izquierda">
+
+            </div>
+        </nav>
+
+    </header>
+    <nav class="sidebar">
+    
+    </nav>
 </body>
 </html>
