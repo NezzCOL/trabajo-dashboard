f3fd2cf1f9065666c0f24a28af705b8cac6b9d08
NezzCOL <jhonhernandezospino@gmail.com>
2023-08-30 15:18:43 -0500

    trabajo-dashboard

--- pagina/pagina-inicio.php
+++ pagina/pagina-inicio.php
@@ -4,12 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../css/style-pagina-inicio.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body>
-    <header class="content">
-        <nav class="navbar">
-            
+    <header class="content-navbar">
+        <nav class="navbar">  
+            <button class="button-icon"><i class='bx bx-menu'></i></button>
         </nav>
     </header>
+    <aside class="content-sidebar">
+        <nav class="sidebar">
+            <button class="text-1"><p>INICIO</p></button>
+
+            <button class="text-2"><p>Registrar Usuario</p></button>
+        </nav>
+    </aside>
 </body>
 </html>
